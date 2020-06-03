cc67ba587821238e3a432fa46b8049bbf40b9def
Maxim <64529642+yam86rus@users.noreply.github.com>
2020-06-03 16:24:56 +0500

    react app

---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import classes from './Quiz.module.css'
+import ActiveQuize from '../../components/ActiveQuiz/ActiveQuiz'
+
+class Quiz extends Component {
+    state = {
+        quiz: []
+    }
+    render() {
+        return(
+            <div className={classes.Quiz}>
+                
+                <div className={classes.QuizWrapper}>
+                    <h1>Quiz</h1>
+                    <ActiveQuize />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Quiz
