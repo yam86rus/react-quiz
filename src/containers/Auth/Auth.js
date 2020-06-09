b64a6de2190176e5b1cbed577a5ae06b444ae5b8
Maxim <64529642+yam86rus@users.noreply.github.com>
2020-06-09 13:53:27 +0500

    add input

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import classes from './Auth.module.css'
 import Button from '../../components/UI/Button/Button'
-import Input from '../../components/UI/Input/input'
+import Input from '../../components/UI/Input/Input'
 import is from 'is_js'
 
 
