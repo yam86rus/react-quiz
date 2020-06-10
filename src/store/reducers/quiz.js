72676e7a17c63bf1148043afed27627a82148246
Maxim <64529642+yam86rus@users.noreply.github.com>
2020-06-10 19:14:16 +0500

    up

--- src/store/reducers/quiz.js
+++ src/store/reducers/quiz.js
@@ -1,4 +1,13 @@
-import {FETCH_QUIZES_START,FETCH_QUIZES_SUCCESS,FETCH_QUIZES_ERROR,FETCH_QUIZ_SUCCESS} from '../actions/actionTypes'
+import {
+    FETCH_QUIZES_START,
+    FETCH_QUIZES_SUCCESS,
+    FETCH_QUIZES_ERROR,
+    FETCH_QUIZ_SUCCESS,
+    QUIZ_SET_STATE,
+    FINISH_QUIZ,
+    QUIZ_NEXT_QUESTION,
+    QUIZ_RETRY
+} from '../actions/actionTypes'
 
 const initialState = {
     quizes: [],
@@ -28,6 +37,26 @@ export default function quizReducer(state = initialState,action){
         case FETCH_QUIZ_SUCCESS:
             return {
                 ...state, loading:false, quiz:action.quiz
+            }
+        case QUIZ_SET_STATE:
+            return {
+                ...state,answerState: action.answerState, results: action.results
+            }
+        case FINISH_QUIZ:
+            return {
+                ...state, isFinished: true
+            }
+        case QUIZ_NEXT_QUESTION:
+            return {
+                ...state,answerState: null, activeQuestion: action.number
+            }
+        case QUIZ_RETRY:
+            return{
+                ...state,
+                activeQuestion: 0,
+                answerState: null,
+                isFinished: false,
+                results: {}
             }
         default:
             return state
