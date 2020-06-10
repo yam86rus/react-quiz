6972e0c0b066d33bc8383ddab0fece83f5beeabc
Maxim <64529642+yam86rus@users.noreply.github.com>
2020-06-10 18:16:26 +0500

    up

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -4,16 +4,11 @@ import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 import FinishedQuiz from '../../components/FinishedQuiz/FinishedQuiz'
 import axios from '../../axios/axios-quiz'
 import Loader from '../../components/UI/Loader/Loader'
+import {connect} from 'react-redux'
+import {fetchQuizById} from '../../store/actions/quiz'
 
 class Quiz extends Component {
-  state = {
-    results: {},
-    isFinished: false,
-    activeQuestion: 0,
-    answerState: null,
-    quiz: [],
-    loading: true
-  }
+
 
   onAnswerClickHandler = answerId => {
     if (this.state.answerState) {
@@ -71,18 +66,8 @@ class Quiz extends Component {
     })
   }
 
-  async componentDidMount() {
-    try {
-      const response = await axios.get(`/quizes/${this.props.match.params.id}.json`)
-      const quiz = response.data
-
-      this.setState({
-        quiz,
-        loading: false
-      })
-    } catch (e) {
-      console.log(e)
-    }
+  componentDidMount() {
+   this.props.fetchQuizById(this.props.match.params.id)
   }
 
   render() {
@@ -92,21 +77,21 @@ class Quiz extends Component {
           <h1>Ответьте на все вопросы</h1>
 
           {
-            this.state.loading
+            this.props.loading || !this.props.quiz
              ? <Loader />
-             : this.state.isFinished
+             : this.props.isFinished
               ? <FinishedQuiz
-                results={this.state.results}
-                quiz={this.state.quiz}
+                results={this.props.results}
+                quiz={this.props.quiz}
                 onRetry={this.retryHandler}
               />
               : <ActiveQuiz
-                answers={this.state.quiz[this.state.activeQuestion].answers}
-                question={this.state.quiz[this.state.activeQuestion].question}
+                answers={this.props.quiz[this.props.activeQuestion].answers}
+                question={this.props.quiz[this.props.activeQuestion].question}
                 onAnswerClick={this.onAnswerClickHandler}
-                quizLength={this.state.quiz.length}
-                answerNumber={this.state.activeQuestion + 1}
-                state={this.state.answerState}
+                quizLength={this.props.quiz.length}
+                answerNumber={this.props.activeQuestion + 1}
+                state={this.props.answerState}
               />
 
           }
@@ -116,5 +101,20 @@ class Quiz extends Component {
   }
 }
 
+function mapStateToProps(state) {
+  return {
+    results: state.quiz.results,
+    isFinished: state.quiz.isFinished,
+    activeQuestion: state.quiz.activeQuestion,
+    answerState: state.quiz.answerState,
+    quiz: state.quiz.quiz,
+    loading: state.quiz.loading
+  }
+}
 
-export default Quiz
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchQuizById: id => dispatch (fetchQuizById(id))
+  }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(Quiz)
--- src/store/actions/quiz.js
+++ src/store/actions/quiz.js
@@ -1,5 +1,10 @@
 import axios from '../../axios/axios-quiz'
-import {FETCH_QUIZES_START,FETCH_QUIZES_SUCCESS,FETCH_QUIZES_ERROR} from './actionTypes'
+import {
+    FETCH_QUIZES_START,
+    FETCH_QUIZES_SUCCESS,
+    FETCH_QUIZES_ERROR,
+    FETCH_QUIZ_SUCCESS
+} from './actionTypes'
 
 export function fetchQuizes() {
     return async dispatch => {
@@ -18,6 +23,28 @@ export function fetchQuizes() {
         catch(e) {
             dispatch(fetchQuizesError(e))
         }
+    }
+}
+
+export function fetchQuizById(quizId) {
+    return async dispatch => {
+        dispatch(fetchQuizesStart())
+        
+        try {
+            const response = await axios.get(`/quizes/${quizId}.json`)
+            const quiz = response.data
+      
+            dispatch(fetchQuizSuccess(quiz))
+          } catch (e) {
+            dispatch(fetchQuizesError(e))
+          }
+    }
+}
+
+export function fetchQuizSuccess(quiz) {
+    return {
+        type: FETCH_QUIZ_SUCCESS,
+        quiz
     }
 }
 
